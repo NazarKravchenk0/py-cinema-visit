94693d545dc5ffe0921a953b2d10601ffc9b773b
nazarkravchenko <nazarnazarik5@gmail.com>
2026-01-29 16:40:55 +0200

    check1

--- app/main.py
+++ app/main.py
@@ -1,27 +1,30 @@
+from typing import Dict, List
+
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(movie, customers, hall_number, cleaner):
-    # Create Customer instances
+def cinema_visit(
+    movie: str,
+    customers: List[Dict[str, str]],
+    hall_number: int,
+    cleaner: str,
+) -> None:
     customer_objects = [
         Customer(name=customer["name"], food=customer["food"])
         for customer in customers
     ]
 
-    # Sell food to customers
     for customer in customer_objects:
         CinemaBar.sell_product(product=customer.food, customer=customer)
 
-    # Create cinema hall and cleaner
     hall = CinemaHall(hall_number=hall_number)
     cleaning_staff = Cleaner(name=cleaner)
 
-    # Start movie session
     hall.movie_session(
         movie_name=movie,
         customers=customer_objects,
-        cleaning_staff=cleaning_staff
+        cleaning_staff=cleaning_staff,
     )
